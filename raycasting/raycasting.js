16cf176a305d2b981bf9d0c67cf901a470634c13
Lucas Vieira <lucasvg@poli.ufrj.br>
2019-04-05 08:50:40 -0300

    Implements state machine

--- raycasting/raycasting.js
+++ raycasting/raycasting.js
@@ -1,7 +1,25 @@
+let STATES = {
+  CREATING_POLYGOM: 1,
+  TO_CREATE_POLYGOM: 2
+};
+
+let currentState = STATES.TO_CREATE_POLYGOM;
+
 function setup(){
   createCanvas(400, 400);
 }
 
 function mouseClicked(){
-	alert('a');
+  switch(currentState) {
+    case STATES.TO_CREATE_POLYGOM:
+      currentState = STATES.CREATING_POLYGOM;
+      alert('TO_CREATE_POLYGOM');
+      alert(mouseX + ' ' + mouseY);
+      break;
+    case STATES.CREATING_POLYGOM:
+      alert('CREATING_POLYGOM');
+      break;
+    default:
+      alert('BUG: SHOULD NOT ENTER HERE');
+  }
 }
