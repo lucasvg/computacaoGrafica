9c993b22d201a46c3dcaac022116f5c30983cd56
Lucas Vieira <lucasvg@poli.ufrj.br>
2019-04-05 17:03:24 -0300

    Adds dots and polygoms

--- raycasting/raycasting.js
+++ raycasting/raycasting.js
@@ -3,7 +3,23 @@ let STATES = {
   TO_CREATE_POLYGOM: 2
 };
 
+let Dot = class Dot {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+let Polygram = class Polygram {
+  dots = [];
+  addDot(dot){
+    this.dots.push(dot);
+  }
+}
+
 let currentState = STATES.TO_CREATE_POLYGOM;
+
+let curUnfinishedPolygom;
 
 function setup(){
   createCanvas(400, 400);
@@ -13,6 +29,12 @@ function mouseClicked(){
   switch(currentState) {
     case STATES.TO_CREATE_POLYGOM:
       currentState = STATES.CREATING_POLYGOM;
+
+      curUnfinishedPolygom = new Polygram();
+
+      let dot = new Dot(mouseX, mouseY);
+      curUnfinishedPolygom.addDot(dot);
+
       alert('TO_CREATE_POLYGOM');
       alert(mouseX + ' ' + mouseY);
       break;
